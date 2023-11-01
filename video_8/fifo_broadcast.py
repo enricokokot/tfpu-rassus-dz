9104afd72ec25843dfbaf68fa6b66031ae83a0a7
coco <kokotenrico@gmail.com>
2023-11-01 10:55:14 +0100

    examples from video 08

---
+++ video_8/fifo_broadcast.py
@@ -0,0 +1,47 @@
+import asyncio
+
+NODES = 10
+sequences = [0]*NODES
+buffer = {}
+
+
+async def handle_connection(reader, writer):
+    print("Someone is here")
+
+    while data := await reader.readline():
+        try:
+            ldata = data.decode("utf-8").split(",", maxsplit=2)
+            sender, seq, msg = int(ldata[0]), int(ldata[1]), ldata[2].strip()
+
+            print("Received:", sender, seq, msg)
+            buffer[sender, seq] = msg
+
+            # find a message to deliver
+            def deliver():
+                print(f"Status: ", "-".join(map(str, sequences)))
+                for n, x in enumerate(sequences):
+                    if (n, x) in buffer:
+                        print(
+                            f"Delivering (node={n}, seq={x}): ", buffer[n, x])
+                        del buffer[n, x]
+                        sequences[n] += 1
+                        return True
+
+            while deliver():
+                pass
+
+        except Exception as e:
+            print(e)
+
+
+async def main():
+    server = await asyncio.start_server(handle_connection, "127.0.0.1", 9000)
+
+    print(server.sockets)
+
+    async with server:
+        await server.serve_forever()
+
+asyncio.run(main())
+
+# ncat 127.0.0.1 9000
---
+++ video_8/tcp.py
@@ -0,0 +1,29 @@
+import asyncio
+
+
+async def handler(reader, writer):
+    print("Nova konekcija!")
+
+    writer.write(b"Hello!\n")
+    await writer.drain()
+
+    while True:
+        l = await reader.readline()
+        print(l.decode("utf-8"))
+
+
+async def main():
+    server = await asyncio.start_server(
+        handler,  # funkcija koja će zapriamti nove konekcije
+        "127.0.0.1",
+        8080
+    )
+
+    async with server:
+        print("Server started")
+        await server.serve_forever()
+
+
+asyncio.run(main())
+
+# nodemon -e py -x python tcp.py
