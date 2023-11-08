ea74a63b9c1507f3fbff6c9593045d437d00277c
enricokokot <51400198+enricokokot@users.noreply.github.com>
2023-11-08 12:19:14 +0100

    completed first half of homework for 8.11.

--- priprema_03.py
+++ priprema_03.py
@@ -3,6 +3,51 @@ import aiohttp
 import json
 
 
+# 01
+async def print_hello_with_delay():
+    random_delay = random.randint(1, 50) / 10
+    print("Hello world!")
+    print(f"We'll wait exactly {random_delay} seconds")
+    await asyncio.sleep(random_delay)
+
+# asyncio.run(print_hello_with_delay())
+
+
+# 02
+async def zadatak1():
+    for percentage in range(0, 110, 10):
+        await asyncio.sleep(1)
+        print(f"Task 1 completed {percentage}%")
+
+async def zadatak2():
+    for percentage in range(0, 110, 10):
+        await asyncio.sleep(1.5)
+        print(f"Task 2 completed {percentage}%")
+
+async def main():
+    task1 = asyncio.create_task(zadatak1())
+    task2 = asyncio.create_task(zadatak2())
+
+    # await asyncio.gather(task1, task2)
+    await task1
+    await task2
+
+# asyncio.run(main())
+
+
+# 03
+async def timeout_example():
+    try:
+        await asyncio.wait_for(function_to_call(), timeout=3)
+    except asyncio.TimeoutError:
+        print("There was a TimeoutError!")
+
+async def function_to_call():
+    await asyncio.sleep(3.1)
+
+# asyncio.run(timeout_example())
+
+
 # 04
 async def fetch_url(url):
     await asyncio.sleep(2)
@@ -10,7 +55,6 @@ async def fetch_url(url):
         async with session.get(url) as response:
             result = await response.text()
             print(f"Length of response for {url} is: {len(result)}")
-
 
 async def main():
     urls = [
@@ -38,7 +82,6 @@ async def get_json(url):
             # print(json.dumps(result, indent=2))
             print(f'{{\n    "userId": {result["userId"]}\n    "id": {
                   result["id"]}\n    "title": {result["title"]}\n    "body": {result["body"]}\n}}')
-
 
 async def main():
     urls = [
